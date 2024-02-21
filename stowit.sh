f3ebe168783bf416063d48f4b47819bab3a87373
bcherukuri <bcherukuri@carbon3d.com>
2024-02-21 09:25:41 -0800

    fixing p10k

--- stowit.sh
+++ stowit.sh
@@ -89,7 +89,7 @@ echo "Starting to stow dotfiles..."
 # Ensure the script runs from the directory where it's located
 cd "$(dirname "$0")"
 ## declare an array variable
-declare -a arr=("nvim" "tmux" "zsh" "picom" "polybar" "alacritty" "fontconfig" "variety" "i3")
+declare -a arr=("nvim" "tmux" "zsh" "picom" "polybar" "alacritty" "fontconfig" "variety" "i3" "p10k")
 
 ## now loop through the above array
 for pkg in "${arr[@]}"
