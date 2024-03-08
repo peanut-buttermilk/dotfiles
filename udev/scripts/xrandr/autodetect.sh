a295a4c38e96980dafa88d033c71043afab4e5dd
Balaji Cherukuri <balusoft@gmail.com>
2024-03-08 09:38:35 -0800

    improving the xrandr autodetect

--- udev/scripts/xrandr/autodetect.sh
+++ udev/scripts/xrandr/autodetect.sh
@@ -13,16 +13,25 @@ autodetect() {
     MONITORS=$(xrandr --query | grep " connected" | sort | cut -d' ' -f1 | tr -d '\n')
     SHA=$(hwinfo --monitor | awk -F': ' '/Unique ID:/{print $2}' | sort | tr -d '\n' | sha256sum | awk '{print $1}')       
 
+    CONFIG_ANY=$(ls -rt ${DOTFILES}/udev/scripts/xrandr/config/${MONITORS}-*.sh | tail -1)
     CONFIG=${DOTFILES}/udev/scripts/xrandr/config/${MONITORS}-${SHA}.sh
     
-    if [ ! -f $CONFIG ]; then
-        echo "$CONFIG doesn't exist, running xrandr --auto"
-        echo "#!/bin/sh -x" >> $CONFIG
-        echo "xrandr --auto" >> $CONFIG
-        chmod +x $CONFIG
+    if [ ! -f "$CONFIG" ]; then
+        if [ "$MONITORS" = "" ]; then
+            xrandr --auto
+            return
+        elif [ -n "$CONFIG_ANY" ]; then
+            cp "$CONFIG_ANY" "$CONFIG"
+        else
+            echo "$CONFIG doesn't exist, running xrandr --auto"
+            echo "#!/bin/sh -x" >> "$CONFIG"
+            echo "xrandr --auto" >> "$CONFIG"
+            chmod +x "$CONFIG"
+        fi
     fi
 
-    $CONFIG
+    "$CONFIG"
+
 }
 
 reload_i3() {
