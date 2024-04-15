40e87227d157f50f00273cab1884194e43464169
Balaji Cherukuri <balusoft@gmail.com>
2024-04-15 11:28:10 -0700

    adding swaylock image

--- hypr/.config/hypr/scripts/sleep.sh
+++ hypr/.config/hypr/scripts/sleep.sh
@@ -3,4 +3,4 @@ swayidle -w timeout 300 'swaylock -f -c 000000' \
             timeout 600 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
             timeout 900 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
+            before-sleep 'swaylock -f -c 000000 -i ~/.config/wallpapers/0009.jpg' &
